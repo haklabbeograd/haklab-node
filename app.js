d4f1d1e9ebfd86904897638bc8a6a05eec300b91
dstankovic <dusanstankovic90@gmail.com>
2015-08-24 21:48:40 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+// var _ = require('underscore');
+var cors = require('cors');
+
+var app = express();
+
+
+app.use(cors());
+
+app.get('/',function(req,res) {
+  res.send("Hello");
+});
+
+var currentId = 3;
+var radionice = [
+  { id: 0, ime: "javascript", mesto: "hklbgd"},
+  { id: 1, ime: "os", mesto: "hklbgd"},
+  { id: 2, ime: "elektronika", mesto: "hklbgd"}
+];
+
+
+app.get('/radionice', function(req,res){
+  setTimeout(function(){
+    res.json(radionice);
+  }, 3000);
+});
+
+app.get('/radionice/:id',function(req,res){
+    var id = Number(req.params.id), radionica;
+    //TODO dodaj underscore
+    console.log(id);
+    for(var i in radionice){
+        if(radionice[i].id === id){
+            radionica = radionice[i];
+        }
+    }
+    res.json(radionica);
+});
+
+//TODO zameni sa post
+app.get('/test',function(req,res){
+    var radionica = {
+      id: currentId,
+      ime: req.query.ime,
+      mesto: "haklab"
+    }
+    console.log(radionica);
+    currentId++;
+    radionice.push(radionica);
+});
+
+// HTTP - get (citanje), post (kreiranje), put (modifikacija), delete (brisanje)
+
+
+app.listen(5000,function(){
+  console.log("server has stared on port 5000");
+
+})
